e1002eae568c38834858b18d9b8377e87be1be36
surajkhan pinjar <surajkhanpinjar@gmail.com>
2025-08-01 11:59:06 +0530

    Skeleton Base Creating

---
+++ auto-javadocs/src/main/java/com/aidevassist/auto_javadocs/AutoJavadocsApplication.java
@@ -0,0 +1,13 @@
+package com.aidevassist.auto_javadocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoJavadocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoJavadocsApplication.class, args);
+	}
+
+}
---
+++ auto-javadocs/src/test/java/com/aidevassist/auto_javadocs/AutoJavadocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidevassist.auto_javadocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoJavadocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
