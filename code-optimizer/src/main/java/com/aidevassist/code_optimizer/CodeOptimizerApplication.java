23cd6b326c24c1701dc9a51eafa15276f7d085da
surajkhan pinjar <surajkhanpinjar@gmail.com>
2025-08-01 11:34:19 +0530

    model and Code Optimizer

---
+++ code-optimizer/src/main/java/com/aidevassist/code_optimizer/CodeOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.aidevassist.code_optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeOptimizerApplication.class, args);
+	}
+
+}
---
+++ code-optimizer/src/test/java/com/aidevassist/code_optimizer/CodeOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidevassist.code_optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
