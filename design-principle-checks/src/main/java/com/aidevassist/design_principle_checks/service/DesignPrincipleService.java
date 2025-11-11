13fd482f9174d609e2ffb50c80c13e4a49d78255
surajkhan pinjar <surajkhanpinjar@gmail.com>
2025-11-11 17:13:25 +0530

    Removing uneccessory files

--- design-principle-checks/src/main/java/com/aidevassist/design_principle_checks/service/DesignPrincipleService.java
+++
@@ -1,9 +0,0 @@
-package com.aidevassist.design_principle_checks.service;
-
-
-import com.aidevassist.model.dto.DesignPrincipleRequest;
-import com.aidevassist.model.dto.DesignPrincipleResponse;
-
-public interface DesignPrincipleService {
-    DesignPrincipleResponse aiAnalyzePrinciples(DesignPrincipleRequest request);
-}
